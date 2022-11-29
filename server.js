c240d8225ba8ce8e7cb882da4eff83bcb64c16ed
AdaN6 <a_disney_da@hotmail.com>
2022-11-29 16:52:33 +0100

    fixed bugs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-
 const port = process.env.PORT || 6000;
 
-server.listen(port, () => console.log(`Server connected to port ${port}`))
+app.use(express.json());
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
