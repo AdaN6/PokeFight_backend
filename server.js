ca478e14747956000d1be21f718014b7586f95c6
Quantumminded <lukkino31@gmail.com>
2022-11-30 00:26:55 +0100

    fixing typo

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ let jsonData = require('./pokemon.json');
 app.use('/pokemon', require('./routes/route.js'));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
   });
