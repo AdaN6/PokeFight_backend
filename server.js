6382f2465923d71c329bdfd68a16a11fdfe86857
Quantumminded <lukkino31@gmail.com>
2022-11-29 21:33:47 +0100

    part 1 first 4 piont done

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
-
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
+//import json data
+let jsonData = require('./pokemon.json');
 
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//Create a GET route on /pokemon which gives the complete list of pokemon from the JSON
+app.get('/pokemon', (req, res) => {
+  res.send(jsonData);
+})
+
+// Create a GET route on /pokemon/:id which gives only one pokemon from the JSON thanks to its id
+app.get('/pokemon/:id', (req, res) => {
+    const { id } = req.params;
+    const singlePokemon = jsonData[id - 1];
+    res.send(singlePokemon);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
