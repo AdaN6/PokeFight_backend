752109cf4b179dfbc703481186ed7570304f1a57
Quantumminded <lukkino31@gmail.com>
2022-11-30 16:21:23 +0100

    fist parte done

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+//import controller
 const controller = require('./controller.js');
 
+//all Pokemon route
 router.get('/', controller.allPokemon);
 
+//Pokemon by id route
 router.get('/:id', controller.pokemonById);
 
+//Pokemon Details route
 router.get('/:id/:info', controller.pokemonDetails);
 
 module.exports = router;
